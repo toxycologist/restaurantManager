9f994d444793b62ff4e5450728eb119ee868e895
Marcin Kiepura <marcin.dagger@gmail.com>
2021-02-16 18:23:43 +0100

    dodana obsługa vaadina i podstawowe gui

--- src/main/java/pl/manager/restaurant/MainMenuGui.java
+++ src/main/java/pl/manager/restaurant/MainMenuGui.java
@@ -1,4 +1,22 @@
 package pl.manager.restaurant;
 
-public class MainMenuGui {
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import pl.manager.restaurant.Repo.WorkerRepo;
+
+@Route("main-menu")
+public class MainMenuGui extends VerticalLayout {
+
+    WorkerRepo workerRepo;
+
+    @Autowired
+    public MainMenuGui(WorkerRepo workerRepo){
+        Text textHello = new Text("Witaj w aplikacji pomagającej zarządzać barem lub restauracją! Wybierz opcje z menu poniżej");
+        Button buttonAddWorker = new Button("Dodaj pracownika");
+        add(textHello, buttonAddWorker);
+    }
+
 }
